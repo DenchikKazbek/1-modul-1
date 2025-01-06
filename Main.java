5b4f1d0dd1a07d5ce1e5c62e04a35fdb5c033693
DenchikKazbek <sirtul@yandex.ru>
2025-01-06 16:30:49 +1200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 6;
+        if ((x % 2 + y%2)==0) {
+            System.out.println("Удачное совпадение");
+        }
+    }
+}
